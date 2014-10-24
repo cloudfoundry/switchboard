d675d42791c217611ec6ef664f6397a134d6bd60
David Sabeti and Rob Dimsdale <rdimsdale@pivotal.io>
2014-10-24 15:20:50 -0700

    Actually close http responses

--- cmd/switchboard/healthcheck.go
+++ cmd/switchboard/healthcheck.go
@@ -34,13 +34,13 @@ func (h *HttpHealthcheck) Start(errorCallback func()) {
 			if err != nil {
 				fmt.Printf("Error dialing healthchecker at %s: %v\n", h.getEndpoint(), err.Error())
 			} else {
-				defer resp.Body.Close()
 				switch resp.StatusCode {
 				case http.StatusServiceUnavailable:
 					errorCallback()
 				case http.StatusOK:
 					fmt.Printf("Healthcheck at %s succeeded\n", h.getEndpoint())
 				}
+				resp.Body.Close()
 			}
 			time.Sleep(1 * time.Second)
 		}

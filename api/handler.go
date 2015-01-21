242e627ec778627ef4acca7f4b01c3b5ccf3f10f
Abhijit Hiremagalur and Lyle Franklin <ahiremagalur@pivotal.io>
2015-01-21 12:18:39 -0800

    Refactor middleware; all handlers get basic auth
    [#81149294]

--- api/handler.go
+++ api/handler.go
@@ -1,51 +1,19 @@
 package api
 
 import (
-	"crypto/subtle"
 	"net/http"
 
+	"github.com/pivotal-cf-experimental/switchboard/api/middleware"
 	"github.com/pivotal-cf-experimental/switchboard/domain"
 	"github.com/pivotal-golang/lager"
 )
 
 func NewHandler(backends domain.Backends, logger lager.Logger) http.Handler {
 	mux := http.NewServeMux()
-	mux.Handle("/v0/backends", basicAuthHandler(BackendsIndex(backends)))
+	mux.Handle("/v0/backends", BackendsIndex(backends))
 
-	return panicRecoveryHandler(mux, logger)
-}
-
-func panicRecoveryHandler(next http.Handler, logger lager.Logger) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		defer func() {
-			if panicInfo := recover(); panicInfo != nil {
-				rw.WriteHeader(http.StatusInternalServerError)
-				logger.Error("Panic while serving request", nil, lager.Data{
-					"request":   req,
-					"panicInfo": panicInfo,
-				})
-			}
-		}()
-		next.ServeHTTP(rw, req)
-	})
-}
-
-func basicAuthHandler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		username, password, ok := req.BasicAuth()
-		if ok &&
-			secureCompare(username, "username") &&
-			secureCompare(password, "password") {
-			next.ServeHTTP(rw, req)
-		} else {
-			rw.Header().Set("WWW-Authenticate", "Basic realm=\"Authorization Required\"")
-			http.Error(rw, "Not Authorized", http.StatusUnauthorized)
-		}
-	})
-}
-
-func secureCompare(a, b string) bool {
-	x := []byte(a)
-	y := []byte(b)
-	return subtle.ConstantTimeCompare(x, y) == 1
+	return middleware.Chain{
+		middleware.NewPanicRecovery(logger),
+		middleware.NewBasicAuth(),
+	}.Wrap(mux)
 }
---
+++ api/middleware/basic_auth.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"crypto/subtle"
+	"net/http"
+)
+
+type BasicAuth struct{}
+
+func NewBasicAuth() Middleware {
+	return BasicAuth{}
+}
+
+func (b BasicAuth) Wrap(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		username, password, ok := req.BasicAuth()
+		if ok &&
+			secureCompare(username, "username") &&
+			secureCompare(password, "password") {
+			next.ServeHTTP(rw, req)
+		} else {
+			rw.Header().Set("WWW-Authenticate", "Basic realm=\"Authorization Required\"")
+			http.Error(rw, "Not Authorized", http.StatusUnauthorized)
+		}
+	})
+}
+
+func secureCompare(a, b string) bool {
+	x := []byte(a)
+	y := []byte(b)
+	return subtle.ConstantTimeCompare(x, y) == 1
+}

783aa3c11e0d0634ae14b3e7dbd820c1b64ead13
Rob Dimsdale <rdimsdale@pivotal.io>
2015-03-19 16:02:03 -0700

    Use jasmine-npm-browser module.
    [#90122730]

--- gulptasks.js
+++ gulptasks.js
@@ -81,47 +81,11 @@ gulp.task('lint', function () {
     .pipe(plugins.jshint.reporter('fail'));
 });
 
-gulp.task('spec', function(callback) {
-  runSequence('lint', 'spec', callback);
-});
-
-function jasmineConsoleReporter(port, callback) {
-  var phantomjs = spawn('phantomjs', ['spec/support/console_reporter.js', port], {stdio: 'inherit', env: process.env});
-  phantomjs.on('close', callback);
-}
-
-gulp.task('spec', function(callback) {
-  var port = 8888;
-  lsof.rawTcpPort(port, function(data) {
-    if (data.length) {
-      jasmineConsoleReporter(port, callback);
-    } else {
-      portfinder.getPort(function(err, port) {
-        if (err) return callback(err);
-        var env = Object.assign({}, process.env, {JASMINE_PORT: port});
-        var server = spawn('./node_modules/.bin/gulp', ['jasmine'], {env});
-        server.stdout.on('data', function(data) {
-          var output = data.toString();
-          if (output.includes('Jasmine server listening on ')) {
-            jasmineConsoleReporter(port, function(err) {
-              process.kill(server.pid, 'SIGINT');
-              callback(err);
-            });
-          }
-        });
-      });
-    }
-  });
-});
-
-gulp.task('jasmine-assets', function() {
-  return gulp.src(['spec/spec.js', 'spec/app/**/*_spec.js'])
+function jasmineSpecs(options = {}) {
+  return gulp.src('spec/app/**/*_spec.js')
     .pipe(plugins.cached('jasmine-javascript'))
-    .pipe(plugins.webpack({
+    .pipe(plugins.webpack(Object.assign({
       devtool: 'eval',
-      entry: {
-        spec: `./spec/spec.js`
-      },
       module: {
         loaders: [
           {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?experimental=true'}
@@ -130,24 +94,24 @@ gulp.task('jasmine-assets', function() {
       output: {
         filename: '[name].js'
       },
+      quiet: true,
       watch: true
+    }, options)));
+}
 
-    }))
-    .pipe(gulp.dest('tmp/jasmine'));
+gulp.task('spec', function(callback) {
+  return jasmineSpecs({watch: false})
+    .pipe(plugins.jasmineBrowser.specRunner({console: true}))
+    .pipe(plugins.jasmineBrowser.phantomjs());
 });
 
-gulp.task('jasmine-server', function() {
-  function createServer({port, onReady}) {
-    var app = express();
-    app.use(express.static(__dirname + '/tmp/jasmine'));
-    app.use(express.static(__dirname + '/spec/app/public'));
-    app.listen(port, onReady && function() { onReady(port); });
-    return app;
-  }
-  var port = process.env.JASMINE_PORT || 8888;
-  createServer({port, onReady: port => plugins.util.log(`Jasmine server listening on ${port}`)});
+
+gulp.task('jasmine', function() {
+  return jasmineSpecs()
+    .pipe(plugins.jasmineBrowser.specRunner())
+    .pipe(plugins.jasmineBrowser.server());
 });
 
-gulp.task('jasmine', ['jasmine-assets', 'jasmine-server']);
-
-gulp.task('default', ['spec']);
+gulp.task('default', function(callback) {
+  runSequence('lint', 'spec', callback);
+});

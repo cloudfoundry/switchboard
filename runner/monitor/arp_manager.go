a1ae5798f563ea219fa1aa71cbcee1d0c64b6388
Andrew Crump <acrump@pivotal.io>
2016-10-07 19:05:13 -0700

    Backfill ArpManager tests
    [#130781143]

--- runner/monitor/arp_manager.go
+++ runner/monitor/arp_manager.go
@@ -14,19 +14,31 @@ type ArpManager interface {
 	IsCached(ip string) bool
 }
 
+//go:generate counterfeiter . CmdRunner
+type CmdRunner interface {
+	Run(name string, cmd ...string) ([]byte, error)
+}
+
+type ExecCmdRunner struct{}
+
+func (r *ExecCmdRunner) Run(name string, arg ...string) ([]byte, error) {
+	return exec.Command(name, arg...).CombinedOutput()
+}
+
 type ArpManagerCmd struct {
+	runner CmdRunner
 	logger lager.Logger
 }
 
-func NewArmManager(logger lager.Logger) ArpManager {
+func NewArmManager(runner CmdRunner, logger lager.Logger) ArpManager {
 	return &ArpManagerCmd{
+		runner: runner,
 		logger: logger,
 	}
 }
 
 func (a ArpManagerCmd) ClearCache(ip string) error {
-
-	output, err := exec.Command("arp", "-d", ip).CombinedOutput()
+	output, err := a.runner.Run("arp", "-d", ip)
 
 	if err != nil {
 		return errors.New(fmt.Sprintf("failed to delete arp entry: OUTPUT=%s, ERROR=%s", output, err.Error()))
@@ -36,8 +48,7 @@ func (a ArpManagerCmd) ClearCache(ip string) error {
 }
 
 func (a ArpManagerCmd) IsCached(ip string) bool {
-
-	output, err := exec.Command("arp", ip).CombinedOutput()
+	output, err := a.runner.Run("arp", ip)
 	if err != nil {
 		a.logger.Info(fmt.Sprintf("arp didnt find %s in cache, skipping cache invalidation", ip), lager.Data{
 			"err":    err.Error(),
